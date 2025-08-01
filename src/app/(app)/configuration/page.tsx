bda721edd89c8f9e91defe640b66369164a59698
semer moh <semermoh1@gmail.com>
2025-08-01 19:38:07 +0000

    Each of the following should be implemented as manageable tables with th

--- src/app/(app)/configuration/page.tsx
+++ src/app/(app)/configuration/page.tsx
@@ -9,7 +9,7 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { masterData, setMasterData } from "@/lib/master-data";
-import { toast } from "@/hooks/use-toast";
+import { useToast } from "@/hooks/use-toast";
 
 const configAreas = [
     {
